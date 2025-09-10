945d10a3c74fbab3cf107c3916696dfa0e712664
ACHUSACHU <ACHUSACHU@DESKTOP-IH5DM00>
2025-09-10 12:05:29 +0530

    commit

---
+++ kochi/src/test/java/Demo/Maven2.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class Maven {
+
+	public static void main(String[] args) {
+	
+		System.out.println("kochi");
+		System.out.println("kochi");
+		System.out.println("kochi");
+		System.out.println("kochi");
+		System.out.println("kochi");
+		System.out.println("kochi");
+		System.out.println("kochi");
+		System.out.println("kochi");
+		
+		
+		
+	}
+
+}
